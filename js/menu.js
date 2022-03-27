76fa1ec50b1c906ca2363522b1237b57de1b33bd
Rostyslav Marchenko <marchenko.r.7@gmail.com>
2022-03-28 01:00:22 +0300

    second commit

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,13 @@
   const refs = {
     openMenuBtn: document.querySelector(".menu-open-btn"),
     closeMenuBtn: document.querySelector(".menu-close-btn"),
-    Menu: document.querySelector(".mob-menu"),
+    menu: document.querySelector(".mob-menu"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.Menu.classList.toggle("is-hidden");
+    refs.menu.classList.toggle("is-hidden");
   }
 })();
